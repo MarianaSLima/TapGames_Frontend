bfebe353db6777aeaeea13445cd1eea59b2112b5
Mariana Dos Santos Lima <mariana.lima162@hotmail.com>
2022-08-09 17:02:44 -0300

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://tapgames.herokuapp.com',
     headers: {
         'Content-Type':'application/json'
     }
